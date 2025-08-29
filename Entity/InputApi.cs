a253436d99f5ab8bbbb91edf72df38b7d847a5de
hiennguyen <hiennguyen@strongtie.com>
2025-08-29 13:20:47 +0700

    entity added

---
+++ Entity/InputApi.cs
@@ -0,0 +1,10 @@
+﻿namespace Entity
+{
+	public class InputApi
+	{
+        public string? Store { get; set; }
+        public string? Culture { get; set; }
+        public string? Name { get; set; }
+        public string? UserName { get; set; }
+    }
+}
